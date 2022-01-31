a6a6303e866047257a1589234733cc552a4e4b82
FusterTormo <franfustor@gmail.com>
2022-01-31 12:14:42 +0100

    Corregido mensaje cuando no se encuentran muestras

--- validateDB.py
+++ validateDB.py
@@ -18,17 +18,21 @@ if os.path.isdir(path) :
     p = subprocess.Popen(args, stderr = subprocess.PIPE, stdout = subprocess.PIPE)
     out, err = p.communicate()
     arx = out.decode().strip().split("\n")
-    print("INFO: {} samples found".format(len(arx)))
-    # Contar el numero de genes distintos con variante que tiene la muestra
-    # Contar el numero de variantes que tiene cada gen en cada muestra
-    # Comprobar si en la base de datos los numeros son iguales. Imprimir si hay algo que no cuadra
-    for a in arx :
-        # Contar el numero total de variantes que tiene la muestra
-        cmd = "wc -l {}".format(a)
-        args = shlex.split(cmd)
-        p = subprocess.Popen(args, stdout = subprocess.PIPE)
-        out, err = p.communicate()
-        print(out.decode())
+    if arx[0] == "" :
+        print("ERROR: No samples found")
+        sys.exit()
+    else :
+        print("INFO: {} samples found".format(len(arx)))
+        # Contar el numero de genes distintos con variante que tiene la muestra
+        # Contar el numero de variantes que tiene cada gen en cada muestra
+        # Comprobar si en la base de datos los numeros son iguales. Imprimir si hay algo que no cuadra
+        for a in arx :
+            # Contar el numero total de variantes que tiene la muestra
+            cmd = "wc -l {}".format(a)
+            args = shlex.split(cmd)
+            p = subprocess.Popen(args, stdout = subprocess.PIPE)
+            out, err = p.communicate()
+            print(out.decode())
 else :
     print("ERROR: Cannot find {} folder".format(path))
     sys.exit(1)
