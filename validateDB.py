5083b5ede9166831fe7773478c8321ac8c6c84b6
FusterTormo <franfustor@gmail.com>
2022-01-31 12:40:37 +0100

    Añadidos comandos para buscar la informacion en la base de datos

--- validateDB.py
+++ validateDB.py
@@ -23,8 +23,6 @@ if os.path.isdir(path) :
         sys.exit()
     else :
         print("INFO: {} samples found".format(len(arx)))
-        # Contar el numero de variantes que tiene cada gen en cada muestra
-        # Comprobar si en la base de datos los numeros son iguales. Imprimir si hay algo que no cuadra
         for a in arx :
             # Contar el numero total de variantes que tiene la muestra
             cmd = "wc -l {}".format(a)
@@ -32,21 +30,34 @@ if os.path.isdir(path) :
             p = subprocess.Popen(args, stdout = subprocess.PIPE)
             out, err = p.communicate()
             # Leer el nombre de la muestra
-            muestra = arx.split("/")[-3]
+            muestra = a.split("/")[-3]
             print("INFO: Getting data from {}".format(muestra))
-            total = out.decode().strip().split(" ")[0]
-            # Contar el numero de genes distintos con variante que tiene la muestra
+            total = int(out.decode().strip().split(" ")[0]) - 1 # El total de filas que tiene el archivo, menos la primera linea, que es la cabecera
+            # Contar el numero de genes distintos con variante que tiene la muestra (en la clave del diccionario)
+            # Contar el numero de variantes que tiene cada gen en cada muestra (el contenido del diccionario)
             genes = {}
             cmd = "cut -f 7 {}".format(a)
             args = shlex.split(cmd)
             p = subprocess.Popen(args, stdout = subprocess.PIPE)
             out, err = p.communicate()
             for b in out.decode().strip().split("\n") :
-                if b in genes :
-                    genes[b] += 1
-                else :
-                    genes[b] = 1
-            print(genes)
+                if b != "Gene.refGene" :
+                    if b in genes :
+                        genes[b] += 1
+                    else :
+                        genes[b] = 1
+            # Comprobar si en la base de datos los numeros son iguales. Imprimir si hay algo que no cuadra
+            print("INFO: Comparing the information obtained with the stored in the database")
+            sql = "SELECT count(*) FROM run WHERE id_mostra='{}'".format(sample)
+            dbcon = mysql.connector.connect(host="localhost", user="ffuster", password="Aetaeb6e", database="MDSvar")
+            with dbcon as con:
+                with con.cursor() as cur :
+                    cur.execute(sql)
+                    res = cur.fetchall()
+                    print(sql)
+                    print(res)
+
+
 
 else :
     print("ERROR: Cannot find {} folder".format(path))
