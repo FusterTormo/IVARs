2f0ecbb8b5ff938b54a6a723d87ebaaa25bc0f6e
FusterTormo <franfustor@gmail.com>
2022-01-31 12:26:31 +0100

    Añadidos comandos para contar cuantas variantes se han reportado en cada gen

--- validateDB.py
+++ validateDB.py
@@ -13,7 +13,7 @@ Validar que las variantes introducidas en la base de datos son las mismas que ha
 path = input("INPUT: Folder you want to validate: ")
 if os.path.isdir(path) :
     # Buscar cuantas muestras hay en la carpeta que se ha introducido
-    cmd = "find {} -name raw.hg19_mutianno.txt".format(path)
+    cmd = "find {} -name raw.hg19_multianno.txt".format(path)
     args = shlex.split(cmd)
     p = subprocess.Popen(args, stderr = subprocess.PIPE, stdout = subprocess.PIPE)
     out, err = p.communicate()
@@ -23,7 +23,6 @@ if os.path.isdir(path) :
         sys.exit()
     else :
         print("INFO: {} samples found".format(len(arx)))
-        # Contar el numero de genes distintos con variante que tiene la muestra
         # Contar el numero de variantes que tiene cada gen en cada muestra
         # Comprobar si en la base de datos los numeros son iguales. Imprimir si hay algo que no cuadra
         for a in arx :
@@ -32,7 +31,23 @@ if os.path.isdir(path) :
             args = shlex.split(cmd)
             p = subprocess.Popen(args, stdout = subprocess.PIPE)
             out, err = p.communicate()
-            print(out.decode())
+            # Leer el nombre de la muestra
+            muestra = arx.split("/")[-3]
+            print("INFO: Getting data from {}".format(muestra))
+            total = out.decode().strip().split(" ")[0]
+            # Contar el numero de genes distintos con variante que tiene la muestra
+            genes = {}
+            cmd = "cut -f 7 {}".format(a)
+            args = shlex.split(cmd)
+            p = subprocess.Popen(args, stdout = subprocess.PIPE)
+            out, err = p.communicate()
+            for b in out.decode().strip().split("\n") :
+                if b in genes :
+                    genes[b] += 1
+                else :
+                    genes[b] = 1
+            print(genes)
+
 else :
     print("ERROR: Cannot find {} folder".format(path))
     sys.exit(1)
